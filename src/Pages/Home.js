9391562f92870cb7becba6a854ec24c000ac1a7a
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-04-12 19:59:26 +0800

    Modify navigate code

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {useNavigate, useLocation} from "react-router-dom"
 
 export default function Home() {
     const navigate = useNavigate();
     const location = useLocation();
-    console.log(location)
-    if (!location.state){     
-        return navigate("/login")
-    }
 
-    const {_id, name, hkid} = location.state
+ 
+        useEffect(() => {
+            if (!location.state){     
+                return navigate("/login")
+            }
 
-    return (
-        <div>
-            Welcome {name}
-            Your HKID is retrieved: {hkid}
-        </div>        
-    )
+            const {_id, name, hkid} = location.state
+
+            return (
+                <div>
+                    Welcome {name}
+                    Your HKID is retrieved: {hkid}
+                </div>        
+            )
+        })
+        
+    
+
+    
 }
