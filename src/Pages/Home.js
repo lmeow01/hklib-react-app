629974baf2900238204c8a0d04614c402756c8cc
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-04-12 20:08:21 +0800

    zzz

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,17 +12,14 @@ export default function Home() {
         if (!location.state){     
             return navigate("/login")
         }
-    })
 
-    if (location.state){
         setName(location.state.name)
         setHkid(location.state.hkid)
         setId(location.state._id)
-        
-    }
+    })
+
     return (
         (name.length != 0 && hkid.length != 0 && <div>
-            
             Welcome {name}
             Your HKID is retrieved: {hkid}
         </div>  )
