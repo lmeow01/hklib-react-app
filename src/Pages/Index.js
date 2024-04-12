9b2152a69fb555aca434c9e0f0112a5af377633c
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-04-12 23:21:31 +0800

    Change render condition

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -5,14 +5,22 @@ import {useCookies} from "react-cookie"
 export default function Index() {
     const navigate = useNavigate();
     const [cookies, setCookie, removeCookie] = useCookies(['_id', 'name', 'hkid'])
+    const [name, setName] = useState("")
+    const [hkid, setHkid] = useState("")
     console.log(cookies)
+    useEffect(() => {
+        if (cookies.name && cookies.name.length != 0) {
+            setName(cookies.name)
+            setHkid(cookies.hkid)
+        }
+    })
     return (
         <>
-            {!cookies.name && <Navigate to="/login" reaplce={true}/> }
-            {cookies.name && 
+            {hkid.length == 0 && <Navigate to="/login" reaplce={true}/> }
+            {hkid.length != 0 && 
                 <div className="flex">
-                    <h1>Your Name is {cookies.name}</h1>
-                    <h1>Your HKID is retrived as follow: {cookies.hkid}</h1>
+                    <h1>Your Name is {name}</h1>
+                    <h1>Your HKID is retrived as follow: {hkid}</h1>
                     <Link to="/login" onClick={() => {
                         removeCookie("_id")
                         removeCookie("name")
