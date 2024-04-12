5ff2abcd5a9741a68b8b341e4826c739eabd4988
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-04-13 00:56:03 +0800

    center the login form

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,7 +3,7 @@ import { TEInput, TERipple } from "tw-elements-react";
 
 export default function Login() {
   return (
-    <section className="h-screen">
+    <section className="h-screen flex flex-col items-center">
       <div className="container h-full px-6 py-24">
         <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
           {/* <!-- Left column container with background--> */}
